ba5af1425b729e653f27d91d4e0e0b5037dddcee
Abel Guillen <abel.guillenfuerte@thoughtworks.com>
2022-11-17 20:48:10 +0100

    Tune repository to my own needs

--- ha_server/backend.tf
+++ ha_server/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "aa-terraform-state-160071257600"
-    key            = "terraform/aida-abel.tfstate"
+    bucket         = "abel-terraform-state-160071257600"
+    key            = "terraform/abel.tfstate"
     #dynamodb_table = "s3backend" # LockID configuration
     region         = "eu-west-1"
   }
