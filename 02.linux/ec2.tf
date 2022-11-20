43a27396cc25fb463c5c1af3a7b11899921dcb00
Abel Guillen <abel.guillenfuerte@thoughtworks.com>
2022-11-20 22:29:40 +0100

    Open port 80 as well

--- 02.linux/ec2.tf
+++ 02.linux/ec2.tf
@@ -10,6 +10,15 @@ resource "aws_security_group" "abel-linux-sg" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow incoming SSH connections"
   }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming SSH connections"
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
